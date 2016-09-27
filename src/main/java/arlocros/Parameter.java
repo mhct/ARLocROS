b34d97bf36d3b2f6a702e97aac8743c73ba4efa1
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-09-27 15:20:20 +0200

    added useThreshold to Parameter

--- src/main/java/arlocros/Parameter.java
+++ src/main/java/arlocros/Parameter.java
@@ -3,69 +3,73 @@ package arlocros;
 import com.google.auto.value.AutoValue;
 import org.ros.node.parameter.ParameterTree;
 
-/**
- * @author Hoang Tung Dinh
- */
+/** @author Hoang Tung Dinh */
 @AutoValue
 public abstract class Parameter {
 
-    protected Parameter() {}
+  protected Parameter() {}
 
-    public abstract String patternDirectory();
+  public abstract String patternDirectory();
 
-    public abstract String markerFrameName();
+  public abstract String markerFrameName();
 
-    public abstract String cameraFrameName();
+  public abstract String cameraFrameName();
 
-    public abstract String cameraImageTopic();
+  public abstract String cameraImageTopic();
 
-    public abstract String cameraInfoTopic();
+  public abstract String cameraInfoTopic();
 
-    public abstract String markerConfigFile();
+  public abstract String markerConfigFile();
 
-    public abstract boolean badPoseReject();
+  public abstract boolean badPoseReject();
 
-    public abstract String poseTopicName();
+  public abstract String poseTopicName();
 
-    public abstract boolean visualization();
+  public abstract boolean visualization();
 
-    public static Parameter createFromParameterTree(ParameterTree parameterTree) {
-        return builder().patternDirectory(parameterTree.getString("/ARLocROS/pattern_dir"))
-                .markerConfigFile(parameterTree.getString("/ARLocROS/marker_config_file"))
-                .markerFrameName(parameterTree.getString("/ARLocROS/marker_frame_name"))
-                .cameraFrameName(parameterTree.getString("/ARLocROS/camera_frame_name"))
-                .cameraImageTopic(parameterTree.getString("/ARLocROS/camera_image_topic"))
-                .cameraInfoTopic(parameterTree.getString("/ARLocROS/camera_info_topic"))
-                .badPoseReject(parameterTree.getBoolean("/ARLocROS/bad_pose_reject"))
-                .poseTopicName(parameterTree.getString("/ARLocROS/pose_topic_name"))
-                .visualization(parameterTree.getBoolean("/ARLocROS/visualization"))
-                .build();
-    }
+  public abstract boolean useThreshold();
 
-    public static Builder builder() {
-        return new AutoValue_Parameter.Builder();
-    }
+  public static Parameter createFromParameterTree(ParameterTree parameterTree) {
+    return builder()
+        .patternDirectory(parameterTree.getString("/ARLocROS/pattern_dir"))
+        .markerConfigFile(parameterTree.getString("/ARLocROS/marker_config_file"))
+        .markerFrameName(parameterTree.getString("/ARLocROS/marker_frame_name"))
+        .cameraFrameName(parameterTree.getString("/ARLocROS/camera_frame_name"))
+        .cameraImageTopic(parameterTree.getString("/ARLocROS/camera_image_topic"))
+        .cameraInfoTopic(parameterTree.getString("/ARLocROS/camera_info_topic"))
+        .badPoseReject(parameterTree.getBoolean("/ARLocROS/bad_pose_reject"))
+        .poseTopicName(parameterTree.getString("/ARLocROS/pose_topic_name"))
+        .visualization(parameterTree.getBoolean("/ARLocROS/visualization"))
+        .useThreshold(parameterTree.getBoolean("/ARLocROS/use_threshold"))
+        .build();
+  }
 
-    @AutoValue.Builder
-    public abstract static class Builder {
-        public abstract Builder patternDirectory(String value);
+  public static Builder builder() {
+    return new AutoValue_Parameter.Builder();
+  }
 
-        public abstract Builder markerFrameName(String value);
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract Builder patternDirectory(String value);
 
-        public abstract Builder cameraFrameName(String value);
+    public abstract Builder markerFrameName(String value);
 
-        public abstract Builder cameraImageTopic(String value);
+    public abstract Builder cameraFrameName(String value);
 
-        public abstract Builder cameraInfoTopic(String value);
+    public abstract Builder cameraImageTopic(String value);
 
-        public abstract Builder markerConfigFile(String value);
+    public abstract Builder cameraInfoTopic(String value);
 
-        public abstract Builder badPoseReject(boolean value);
+    public abstract Builder markerConfigFile(String value);
 
-        public abstract Builder poseTopicName(String value);
+    public abstract Builder badPoseReject(boolean value);
 
-        public abstract Builder visualization(boolean value);
+    public abstract Builder poseTopicName(String value);
 
-        public abstract Parameter build();
-    }
+    public abstract Builder visualization(boolean value);
+
+    public abstract Builder useThreshold(boolean value);
+
+    public abstract Parameter build();
+  }
 }
